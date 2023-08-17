9faa5bab987fd157c6445229ecc03862a2350855
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-08-17 16:05:25 -0300

    Fundação - Percorrendo Arrays

--- fundacao/main.go
+++ fundacao/main.go
@@ -17,5 +17,12 @@ var (
 )
 
 func main() {
-	fmt.Printf("O tipo de E é %T", f)
+	var meuArray [3]int
+	meuArray[0] = 1
+	meuArray[1] = 2
+	meuArray[2] = 3
+
+	for i, v := range meuArray {
+		fmt.Printf("O valor do indice %d e o valor é %d\n", i, v)
+	}
 }
