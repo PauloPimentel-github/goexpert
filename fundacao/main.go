9a35ed21e09d244c19e6c2e1ba36dddfe76828e5
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-08-22 20:40:58 -0300

    Fundação - Condicionais

--- fundacao/main.go
+++ fundacao/main.go
@@ -2,22 +2,22 @@ package main
 
 func main() {
 
-	for i := 0; i < 10; i++ {
-		println(i)
+	a := 4
+	b := 2
+	c := 3
+
+	if a > b && c > a {
+		println("a > b && c > a")
 	}
 
-	numeros := []string{"um", "dois", "três"}
-	for k, v := range numeros {
-		println(k, v)
-	}
-
-	i := 0
-	for i < 10 {
-		println(i)
-		i++
-	}
-
-	for {
-		println("Hello, World!")
+	switch a {
+	case 1:
+		println("a")
+	case 2:
+		println("b")
+	case 3:
+		println("c")
+	default:
+		println("d")
 	}
 }
