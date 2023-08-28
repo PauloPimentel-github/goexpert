7fe69a17ba5fac2f34fc8c71d8c9d2be16e97f23
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-08-28 20:17:53 -0300

    Pacotes importantes - Defer

--- pacotes-importantes/main.go
+++ pacotes-importantes/main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"io"
-	"net/http"
+	"fmt"
 )
 
 func main() {
-	request, err := http.Get("https://www.google.com")
-	if err != nil {
-		panic(err)
-	}
-	response, err := io.ReadAll(request.Body)
-	if err != nil {
-		panic(err)
-	}
-	println(string(response))
-	request.Body.Close()
+	defer fmt.Println("Primeira Linha")
+	fmt.Println("Segunda Linha")
+	fmt.Println("Terceira Linha")
 }
