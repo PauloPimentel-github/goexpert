11ddafbff7d98b5023a601247c6754e5d5027bf7
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-08-29 18:14:17 -0300

    Pacotes importantes - ServeMux

--- pacotes-importantes/main.go
+++ pacotes-importantes/main.go
@@ -1,63 +1,25 @@
 package main
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-)
-
-type ViaCEP struct {
-	Cep         string `json:"cep"`
-	Logradouro  string `json:"logradouro"`
-	Complemento string `json:"complemento"`
-	Bairro      string `json:"bairro"`
-	Localidade  string `json:"localidade"`
-	Uf          string `json:"uf"`
-	Ibge        string `json:"ibge"`
-	Gia         string `json:"gia"`
-	Ddd         string `json:"ddd"`
-	Siafi       string `json:"siafi"`
-}
+import "net/http"
 
 func main() {
-	http.HandleFunc("/", BuscaCepHandler)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Hello World!"))
+	// })
+	mux.HandleFunc("/", HomeHandler)
+	mux.Handle("/blog", blog{title: "My Blog"})
+	http.ListenAndServe(":8080", mux)
 }
 
-func BuscaCepHandler(w http.ResponseWriter, request *http.Request) {
-	if request.URL.Path != "/" {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	cepParams := request.URL.Query().Get("cep")
-	if cepParams == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	cep, error := BuscaCep(cepParams)
-	if error != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(cep)
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
 }
 
-func BuscaCep(cep string) (*ViaCEP, error) {
-	response, error := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
-	if error != nil {
-		return nil, error
-	}
-	defer response.Body.Close()
-	body, error := ioutil.ReadAll(response.Body)
-	if error != nil {
-		return nil, error
-	}
-	var viaCep ViaCEP
-	error = json.Unmarshal(body, &viaCep)
-	if error != nil {
-		return nil, error
-	}
-	return &viaCep, nil
+type blog struct {
+	title string
+}
+
+func (b blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(b.title))
 }
