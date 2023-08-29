bd6cfaa8a3b15c35338e943b5fcd7c50354f1876
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-08-29 16:33:43 -0300

    Pacotes importantes - Busca CEP

--- pacotes-importantes/main.go
+++ pacotes-importantes/main.go
@@ -2,32 +2,52 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
-type Conta struct {
-	Numero int `json:"n"`
-	Saldo  int `json:"s"`
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
 }
 
 func main() {
-	conta := Conta{Numero: 1, Saldo: 100}
-	response, err := json.Marshal(conta)
-	if err != nil {
-		println(err)
-	}
-	println(string(response))
+	for _, cep := range os.Args[1:] {
+		request, err := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer requisição: %v\n", err)
+		}
 
-	err = json.NewEncoder(os.Stdout).Encode(conta)
-	if err != nil {
-		println(err)
-	}
+		defer request.Body.Close()
+		response, err := io.ReadAll(request.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao ler respostas: %v\n", err)
+		}
 
-	jsonPuro := []byte(`{"n":2, "s":200}`)
-	var contaX Conta
-	err = json.Unmarshal(jsonPuro, &contaX)
-	if err != nil {
-		println(err)
+		var data ViaCEP
+		err = json.Unmarshal(response, &data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer parse da resposta: %v \n", err)
+		}
+
+		file, err := os.Create("cidade.txt")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao criar arquivo: %v\n", err)
+		}
+
+		defer file.Close()
+		_, err = file.WriteString(fmt.Sprintf("CEP: %s, Localidade: %s, UF: %s", data.Cep, data.Localidade, data.Uf))
+		fmt.Println("Arquivo criado com sucesso!")
+		fmt.Println("Cidade: ", data.Localidade)
 	}
-	println(contaX.Saldo)
 }
