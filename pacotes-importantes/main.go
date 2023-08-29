09cf591c1263f751d77e8ce0cda295fec8eaa303
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-08-29 16:56:23 -0300

    Pacotes importantes - Iniciando com HTTP

--- pacotes-importantes/main.go
+++ pacotes-importantes/main.go
@@ -1,53 +1,12 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-)
-
-type ViaCEP struct {
-	Cep         string `json:"cep"`
-	Logradouro  string `json:"logradouro"`
-	Complemento string `json:"complemento"`
-	Bairro      string `json:"bairro"`
-	Localidade  string `json:"localidade"`
-	Uf          string `json:"uf"`
-	Ibge        string `json:"ibge"`
-	Gia         string `json:"gia"`
-	Ddd         string `json:"ddd"`
-	Siafi       string `json:"siafi"`
-}
+import "net/http"
 
 func main() {
-	for _, cep := range os.Args[1:] {
-		request, err := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Erro ao fazer requisição: %v\n", err)
-		}
+	http.HandleFunc("/", BuscaCEP)
+	http.ListenAndServe(":8080", nil)
+}
 
-		defer request.Body.Close()
-		response, err := io.ReadAll(request.Body)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Erro ao ler respostas: %v\n", err)
-		}
-
-		var data ViaCEP
-		err = json.Unmarshal(response, &data)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Erro ao fazer parse da resposta: %v \n", err)
-		}
-
-		file, err := os.Create("cidade.txt")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Erro ao criar arquivo: %v\n", err)
-		}
-
-		defer file.Close()
-		_, err = file.WriteString(fmt.Sprintf("CEP: %s, Localidade: %s, UF: %s", data.Cep, data.Localidade, data.Uf))
-		fmt.Println("Arquivo criado com sucesso!")
-		fmt.Println("Cidade: ", data.Localidade)
-	}
+func BuscaCEP(w http.ResponseWriter, request *http.Request) {
+	w.Write([]byte("Hello, world!"))
 }
