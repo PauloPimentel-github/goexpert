18691ebf706d54823e791954ade31c9b204586a0
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-08-29 17:43:53 -0300

    Pacotes importantes - Criando função para BuscaCEP

--- pacotes-importantes/main.go
+++ pacotes-importantes/main.go
@@ -1,6 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
 func main() {
 	http.HandleFunc("/", BuscaCepHandler)
@@ -20,4 +37,22 @@ func BuscaCepHandler(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello, World!"))
+}
+
+func BuscaCep(cep string) (*ViaCEP, error) {
+	response, error := http.Get("https://viacep.com.br/ws" + cep + "/json/")
+	if error != nil {
+		return nil, error
+	}
+	defer response.Body.Close()
+	body, error := ioutil.ReadAll(response.Body)
+	if error != nil {
+		return nil, error
+	}
+	var viaCep ViaCEP
+	error = json.Unmarshal(body, &viaCep)
+	if error != nil {
+		return nil, error
+	}
+	return &viaCep, nil
 }
