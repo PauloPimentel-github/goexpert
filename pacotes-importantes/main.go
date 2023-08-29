d301a35759535a191c0b41910758d19e0d5dd1c5
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-08-29 17:08:46 -0300

    Pacotes importantes - Manipulando Headers

--- pacotes-importantes/main.go
+++ pacotes-importantes/main.go
@@ -3,10 +3,21 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/", BuscaCEP)
+	http.HandleFunc("/", BuscaCepHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func BuscaCEP(w http.ResponseWriter, request *http.Request) {
-	w.Write([]byte("Hello, world!"))
+func BuscaCepHandler(w http.ResponseWriter, request *http.Request) {
+	if request.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	cepParams := request.URL.Query().Get("cep")
+	if cepParams == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello, World!"))
 }
